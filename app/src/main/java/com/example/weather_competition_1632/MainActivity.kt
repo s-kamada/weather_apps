cab8a6009859835c42d8817cea56b5d8ef6ac82c
Shunsuke KAMADA <mod.rab7219@gmail.com>
2022-01-19 19:04:18 +0900

    メインの天気カード枠組み

--- app/src/main/java/com/example/weather_competition_1632/MainActivity.kt
+++ app/src/main/java/com/example/weather_competition_1632/MainActivity.kt
@@ -3,12 +3,19 @@ package com.example.weather_competition_1632
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.weather_competition_1632.ui.theme.Weather_competitionTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,11 +42,25 @@ fun Greeting(name: String) {
 fun WeatherCard(prefectureName: String) {
     Column {
         Text(text = prefectureName)
-        Text(text = "どんよりとした雲")
+        Row(Modifier.padding(4.dp)) {
+            Image(
+                painterResource(R.drawable.ic_cloudy),
+                "cloudy",
+                Modifier.size(24.dp)
+            )
+            Text(text = "どんよりとした雲")
+        }
         Text(text = "-3℃")
         Text(text = "最高 4℃ / 最低 -5℃")
         Text(text = "1013 hPa 69%")
-        Text(text = "南西 0.5 m/s")
+        Row(Modifier.padding(4.dp)) {
+            Image(
+                painterResource(id = R.drawable.ic_wind_soutuheast),
+                "southeast",
+                Modifier.size(8.dp)
+            )
+            Text(text = "南西 0.5 m/s")
+        }
     }
 }
 

0539dd86549c56cd00b3ed3fea0c0b9d48650ed2
Shunsuke KAMADA <mod.rab7219@gmail.com>
2022-01-19 18:47:27 +0900

    モック画像追加

--- app/src/main/java/com/example/weather_competition_1632/MainActivity.kt
+++ app/src/main/java/com/example/weather_competition_1632/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.weather_competition_1632
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -30,10 +31,22 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
+@Composable
+fun WeatherCard(prefectureName: String) {
+    Column {
+        Text(text = prefectureName)
+        Text(text = "どんよりとした雲")
+        Text(text = "-3℃")
+        Text(text = "最高 4℃ / 最低 -5℃")
+        Text(text = "1013 hPa 69%")
+        Text(text = "南西 0.5 m/s")
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     Weather_competitionTheme {
-        Greeting("Android")
+        WeatherCard("青森市")
     }
 }

a1d2e34ea888d75e15d7e0dc60128b20639db79c
Shunsuke KAMADA <mod.rab7219@gmail.com>
2022-01-24 12:29:35 +0900

    背景画像追加

--- app/src/main/java/com/example/weather_competition_1632/MainActivity.kt
+++ app/src/main/java/com/example/weather_competition_1632/MainActivity.kt
@@ -3,10 +3,15 @@ package com.example.weather_competition_1632
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import com.example.weather_competition_1632.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,6 +27,14 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     color = MaterialTheme.colors.background,
                 ) {
+                    // 背景画像を重ねる
+                    Image(
+                        painter = painterResource(id = weatherBusinessModel.weather.background()),
+                        contentDescription = "",
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier.fillMaxSize()
+                    )
+
                     Column(
                         horizontalAlignment = Alignment.CenterHorizontally
                     ) {

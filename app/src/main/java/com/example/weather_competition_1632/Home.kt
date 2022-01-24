b27217ce77a70411856212bfda46da9ce26bdcf6
Shunsuke KAMADA <mod.rab7219@gmail.com>
2022-01-24 12:53:39 +0900

    cardview追加

--- app/src/main/java/com/example/weather_competition_1632/Home.kt
+++ app/src/main/java/com/example/weather_competition_1632/Home.kt
@@ -1,44 +1,56 @@
 package com.example.weather_competition_1632
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.compositeOver
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.weather_competition_1632.ui.theme.*
 
+
 @Composable
 fun WeatherCard(
     weather: WeatherBusinessModel
 ) {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally
+    Card(
+        border = BorderStroke(0.dp, Color.Transparent),
+        backgroundColor = Color(0x66ffffff),
+        elevation = 4.dp,
+        modifier = Modifier.border(0.dp, Color.Transparent)
     ) {
-        Texts.Body(text = weather.location)
-        ImageWithText(
-            image = weather.weather.image(),
-            text = weather.weather.description(),
-            height = 24.dp,
-            style = Texts.Style.BODY
-        )
-        Texts.Header(text = "${weather.temperature} ℃")
-        Texts.Description(text = "最高 ${weather.maxTemp}℃ / 最低 ${weather.minTemp}℃")
-        Texts.Description(text = "${weather.pressure} hPa ${weather.humidity}%")
-        ImageWithText(
-            image = R.drawable.ic_wind_soutuheast,
-            text = "${weather.windDirection} ${weather.windSpeed} m/s",
-            height = 16.dp,
-            style = Texts.Style.DESCRIPTION
-        )
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Texts.Body(text = weather.location)
+            ImageWithText(
+                image = weather.weather.image(),
+                text = weather.weather.description(),
+                height = 24.dp,
+                style = Texts.Style.BODY
+            )
+            Texts.Header(text = "${weather.temperature} ℃")
+            Texts.Description(text = "最高 ${weather.maxTemp}℃ / 最低 ${weather.minTemp}℃")
+            Texts.Description(text = "${weather.pressure} hPa ${weather.humidity}%")
+            ImageWithText(
+                image = R.drawable.ic_wind_soutuheast,
+                text = "${weather.windDirection} ${weather.windSpeed} m/s",
+                height = 16.dp,
+                style = Texts.Style.DESCRIPTION
+            )
+        }
     }
 }
 
@@ -106,7 +118,9 @@ fun WeeklyForecast(
 ) {
 
     LazyColumn(
-        modifier = Modifier.fillMaxWidth().border(1.dp, Color.Black)
+        modifier = Modifier
+            .fillMaxWidth()
+            .border(1.dp, Color.Black)
     ) {
         items(forecast) { forecast ->
             WeekdayForecastCell(forecast = forecast)
